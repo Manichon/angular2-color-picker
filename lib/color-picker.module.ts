6cd2e876dd3bb3d82bc7ce5102b5b33681d5a1ea
DDEAL\Manitra <m.rambelon@deal.fr>
2017-04-07 10:02:07 +0200

    add compiler providers

--- lib/color-picker.module.ts
+++ lib/color-picker.module.ts
@@ -1,13 +1,14 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
+import { COMPILER_PROVIDERS } from '@angular/compiler';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
-import {ColorPickerService} from './color-picker.service';
+import { ColorPickerService } from './color-picker.service';
 import { ColorPickerDirective, DynamicCpModule } from './color-picker.directive';
 
 @NgModule({
     imports: [CommonModule, DynamicCpModule],
-    providers: [ColorPickerService],
+    providers: [ColorPickerService, COMPILER_PROVIDERS],
     declarations: [ColorPickerDirective],
     exports: [ColorPickerDirective]
 })
-export class ColorPickerModule {}
+export class ColorPickerModule { }
